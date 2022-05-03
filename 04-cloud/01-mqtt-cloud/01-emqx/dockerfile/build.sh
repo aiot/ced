aa42767f4ff40f4e0983c84010d47b49c2f36c99
AIOT <aiot@aiot.hk>
2022-05-03 15:23:14 +0800

    tuning emqx dockerfile build.sh

--- 04-cloud/01-mqtt-cloud/01-emqx/dockerfile/build.sh
+++ 04-cloud/01-mqtt-cloud/01-emqx/dockerfile/build.sh
@@ -6,62 +6,38 @@ alpineVersion='{{kubefactory.infraImage.alpine.version}}'
 ubuntuVersion='{{kubefactory.infraImage.ubuntu.version}}'
 imageRepository='{{kubethings.image.repository}}'
 
+buildImage() {
+    sed --in-place \
+        --expression="s/EMQX_VERSION/${emqxVersion}/g" \
+        --expression="s/EMQX_OPERATOR_VERSION/${emqxOperatorVersion}/g" \
+        --expression="s/ALPINE_VERSION/${alpineVersion}/g" \
+        --expression="s/UBUNTU_VERSION/${ubuntuVersion}/g" \
+        $1
+
+    docker build --pull --file="$1" --tag="$2" .
+    if [[ $? != 0 ]]
+    then
+        exit 1
+    fi
+
+    docker push $2
+    docker rmi $2
+}
+
 
 # # build emqx-operator image
-# sed --in-place \
-#     --expression="s/EMQX_OPERATOR_VERSION/${emqxOperatorVersion}/g" \
-#     --expression="s/ALPINE_VERSION/${alpineVersion}/g" \
-#     --expression="s/UBUNTU_VERSION/${ubuntuVersion}/g" \
-#     operator.dockerfile
-
-# docker build --pull --file='operator.dockerfile' --tag="${imageRepository}/emqx-operator:${emqxOperatorVersion}" .
-# if [[ $? != 0 ]]
-# then
-#     exit 1
-# fi
-
-# docker push ${imageRepository}/emqx-operator:${emqxOperatorVersion}
-
+# buildImage operator.dockerfile ${imageRepository}/emqx-operator:${emqxOperatorVersion}
 # docker rmi emqx/emqx-operator-controller:${emqxOperatorVersion}
-# docker rmi ${imageRepository}/emqx-operator:${emqxOperatorVersion}
 
 
 # build emqx image
-sed --in-place \
-    --expression="s/EMQX_VERSION/${emqxVersion}/g" \
-    --expression="s/ALPINE_VERSION/${alpineVersion}/g" \
-    --expression="s/UBUNTU_VERSION/${ubuntuVersion}/g" \
-    emqx.dockerfile
-
-docker build --pull --file='emqx.dockerfile' --tag="${imageRepository}/emqx:${emqxVersion}" .
-if [[ $? != 0 ]]
-then
-    exit 1
-fi
-
-docker push ${imageRepository}/emqx:${emqxVersion}
-
+buildImage emqx.dockerfile docker push ${imageRepository}/emqx:${emqxVersion}
 docker rmi emqx/emqx:${emqxVersion}
-docker rmi ${imageRepository}/emqx:${emqxVersion}
 
 
 # build emqx-edge image
-sed --in-place \
-    --expression="s/EMQX_VERSION/${emqxVersion}/g" \
-    --expression="s/ALPINE_VERSION/${alpineVersion}/g" \
-    --expression="s/UBUNTU_VERSION/${ubuntuVersion}/g" \
-    emqx-edge.dockerfile
-
-docker build --pull --file='emqx-edge.dockerfile' --tag="${imageRepository}/emqx-edge:${emqxVersion}" .
-if [[ $? != 0 ]]
-then
-    exit 1
-fi
-
-docker push ${imageRepository}/emqx-edge:${emqxVersion}
-
+buildImage emqx-edge.dockerfile ${imageRepository}/emqx-edge:${emqxVersion}
 docker rmi emqx/emqx-edge:${emqxVersion}
-docker rmi ${imageRepository}/emqx-edge:${emqxVersion}
 
 
 #
